51eb1114a4d4c73038722185c2c2b2d8b3b9b48a
Juan Pablo Royo Sales <juanpablo.royo@gmail.com>
2018-09-25 18:43:42 +0200

    Wiring controller

--- src/main/java/com/n26/endpoint/TransactionEndpoint.java
+++ src/main/java/com/n26/endpoint/TransactionEndpoint.java
@@ -1,35 +1,52 @@
 package com.n26.endpoint;
 
 import com.n26.model.Statistics;
+import com.n26.model.StoreResult;
 import com.n26.model.Transaction;
+import com.n26.service.StatisticsService;
 import com.n26.service.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TransactionEndpoint {
 
     @Autowired
-    private TransactionService service;
+    private TransactionService transactionService;
+
+    @Autowired
+    private StatisticsService statisticsService;
 
     @RequestMapping(value = "/transactions", method = RequestMethod.POST, consumes = "application/json")
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void save(@RequestBody TransactionRequest transactionRaw){
-        Transaction.create().amount(transactionRaw)
-        service.store()
+    public ResponseEntity save(@RequestBody TransactionRequest transactionRaw){
+        Transaction toStore = Transaction.create()
+                .amount(transactionRaw.getAmount())
+                .timestamp(transactionRaw.getTimestamp().getTime()).build();
+        StoreResult store = transactionService.store(toStore);
+        switch (store){
+            case NO_CONTENT:
+                return new ResponseEntity(HttpStatus.UNPROCESSABLE_ENTITY);
+            case INVALID_TRANSACTION:
+                return new ResponseEntity(HttpStatus.NO_CONTENT);
+            case OK:
+                return new ResponseEntity(HttpStatus.CREATED);
+        }
+        return new ResponseEntity(HttpStatus.CREATED);
     }
 
     @RequestMapping(value = "/statistics", method = RequestMethod.GET)
     @ResponseBody
     public Statistics get(){
-        return Statistics.empty();
+        return statisticsService.get();
     }
 
 
     @RequestMapping(value = "/transactions", method = RequestMethod.DELETE)
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     public void delete(){
-
+        transactionService.deleteAll();
     }
 }
--- src/main/java/com/n26/endpoint/TransactionRequest.java
+++ src/main/java/com/n26/endpoint/TransactionRequest.java
@@ -1,4 +1,32 @@
 package com.n26.endpoint;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
 public class TransactionRequest {
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING)
+    private BigDecimal amount;
+
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss.sss'Z'")
+    public Date timestamp;
+
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }
